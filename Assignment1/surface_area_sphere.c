984222a7b42e9413572a23d2031050e69aae4e7a
gtoshelvin <gtoshelvin@gmail.com>
2025-12-02 07:36:04 +0300

    Add surface area calculation for a sphere

--- Assignment1/surface_area_sphere.c
+++ Assignment1/surface_area_sphere.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    float radius, surfaceArea;
+    float pi = 3.1416;
+
+    printf("Enter the radius of the sphere: ");
+    scanf("%f", &radius);
+
+    surfaceArea = 4 * pi * radius * radius;
+
+    printf("Surface area of the sphere: %.2f\n", surfaceArea);
+
+    return 0;
+}
